9ce1afebe787bde2a41e3d43bc49f84170b2a350
JEREMYMQ <jeremy.jmq2001omg@gmail.com>
2024-05-07 21:55:38 -0500

    añadi index.css

--- codebros-front/src/pages/Landing.tsx
+++ codebros-front/src/pages/Landing.tsx
@@ -3,10 +3,18 @@ import Header from '../components/Header'
 const Landing = () => {
   return (
     <>
-      <Header />
-      <h1>Codebros</h1>
+      <div className='h-screen mx-auto container flex flex-col justify-center items-center'>
+        <Header />
+
+        <h1 className='text-yellow-700'>Codebros</h1>
+      </div>
     </>
   )
 }
 
 export default Landing
+
+
+
+
+
